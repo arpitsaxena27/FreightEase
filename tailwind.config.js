c4bf7f5b5aa8a87a49ed6f3b41e7b291bf06b540
arpitsaxena27 <arpitsaxena2703@gmail.com>
2024-09-05 11:20:06 +0530

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    darkMode: 'class', // Enable class-based dark mode
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}', // Adjust based on your project structure
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
